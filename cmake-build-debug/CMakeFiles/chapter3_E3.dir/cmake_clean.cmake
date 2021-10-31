c0739ac666ff95ba4d85fccac6ccceb2b2865fc5
Kevin <kevin,anjorin@gmx.at>
2021-10-31 22:24:54 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/chapter3_E3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter3_E3.dir/main.c.obj"
+  "chapter3_E3.exe"
+  "chapter3_E3.exe.manifest"
+  "chapter3_E3.pdb"
+  "libchapter3_E3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter3_E3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
